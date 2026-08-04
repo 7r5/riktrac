1efb8d67efc5dd5cab9655721b0c10520cffb150
Ricardo A <fma.7r5@gmail.com>
2026-08-04 17:12:53 -0600

    entry point updated 2

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,4 +6,4 @@ if [ -n "${POSTGRES_CONNECTION_STRING:-}" ] && [ -z "${DATABASE_URL:-}" ]; then
   export DATABASE_URL="$jdbc_url"
 fi
 
-exec "$@"
+exec /opt/traccar/jre/bin/java -XX:+ExitOnOutOfMemoryError "$@"
