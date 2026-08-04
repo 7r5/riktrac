cabb3504d57323288f2319d5f1416c551c9981f6
Ricardo A <fma.7r5@gmail.com>
2026-08-04 17:54:25 -0600

    backend rout updated 3

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,5 +8,13 @@ if [ -n "${POSTGRES_CONNECTION_STRING:-}" ] && [ -z "${DATABASE_URL:-}" ]; then
   export DATABASE_URL="jdbc:postgresql://${hostport}/${DATABASE_NAME}"
 fi
 
+cat > /opt/traccar/conf/traccar.xml <<'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
+<properties>
+  <entry key="config.useEnvironmentVariables">true</entry>
+</properties>
+EOF
+
 cd /opt/traccar
 exec /opt/traccar/jre/bin/java -XX:+ExitOnOutOfMemoryError "$@"
